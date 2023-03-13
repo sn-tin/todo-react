98e992581e6cb19c039c3c580ef20b51d911fd46
Kristine de Jesus <kristinemayadejesus@gmail.com>
2023-03-14 00:02:22 +0800

    fix position

--- src/components/Todo/Todo.styles.js
+++ src/components/Todo/Todo.styles.js
@@ -14,10 +14,7 @@ const StyledTodo = styled.section`
         font-size: clamp(1rem, 2.5vw, 1.25rem);
     }
     @media screen and (min-width: 900px) {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
+        transform: translateX(35%);
     }
 `
 const StyledAddButton = styled.button`
